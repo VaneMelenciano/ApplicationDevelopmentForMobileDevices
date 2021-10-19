3cd68609dbf6778ac7719ee42a4437e7a11c43c6
VaneMelenciano <vanemelenciano@gmail.com>
2021-10-19 14:58:52 -0500

    crear practica 1

---
+++ Practica1/app/src/main/java/com/example/pm_vml_pr01/Ventana2.java
@@ -0,0 +1,14 @@
+package com.example.pm_vml_pr01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ventana2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventana2);
+    }
+}
